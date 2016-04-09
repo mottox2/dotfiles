6f20bd079c2467bd1900414cc0b01050aa1a8ccb
mottox2 <y.mottox2@gmail.com>
2016-04-10 00:39:59 +0900

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 if [ -a $HOME/.vimrc ]; then
-  rm $HOME/.vimrc 
+  rm $HOME/.vimrc
 fi
 ln -s $HOME/dotfiles/.vimrc $HOME/
 
 if [ -a $HOME/.vim ]; then
-  rm -rf $HOME/.vim 
+  rm -rf $HOME/.vim
 fi
 ln -s $HOME/dotfiles/.vim $HOME/
+
+if [ -a $HOME/.config/fish/config.fish ]; then
+  rm $HOME/.config/fish/config.fish
+fi
+ln -f $HOME/dotfiles/fish/config.fish $HOME/.config/fish/
