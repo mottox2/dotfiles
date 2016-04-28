2f92cc461138ad1d5eae7731165f74c278a0daa7
mottox2 <y.mottox2@gmail.com>
2016-04-29 03:18:23 +0900

    Update fish/config

--- fish/config.fish
+++ fish/config.fish
@@ -23,12 +23,14 @@ alias delete-merged-branches="git branch --merged | grep -v \\* | xargs -I % git
 alias gb="git branch | peco |xargs git checkout"
 alias master="git checkout master"
 alias github="hub browse"
+alias gh="github"
 
 # alias for this config file
 alias config="vim ~/.config/fish/config.fish"
 alias reload="source ~/.config/fish/config.fish"
 
 #= envs
+source ~/dotfiles/fish/_envs.fish
 
 # fisherman
 set fisher_home ~/.local/share/fisherman
@@ -43,4 +45,25 @@ function fish_user_key_bindings
 
   # TODO: bash like history using peco
   #bind \cr peco_select_history
+end
+
+# https://github.com/nearengine/fish-title
+function chtc
+  if count $argv > /dev/null
+    if [ (count $argv) = 3 ]
+      echo -n -e "\033]6;1;bg;red;brightness;" $argv[1] "\a"
+      echo -n -e "\033]6;1;bg;green;brightness;" $argv[2] "\a"
+      echo -n -e "\033]6;1;bg;blue;brightness;" $argv[3] "\a"
+    else
+      echo -n -e "\033]6;1;bg;red;brightness;" (echo "ibase=16; " (echo (echo $argv[1] | cut -c 1-2) | tr [a-z] [A-Z]) |bc) "\a"
+      echo -n -e "\033]6;1;bg;green;brightness;" (echo "ibase=16; " (echo (echo $argv[1] | cut -c 3-4) | tr [a-z] [A-Z]) |bc) "\a"
+      echo -n -e "\033]6;1;bg;blue;brightness;" (echo "ibase=16; " (echo (echo $argv[1] | cut -c 5-6) | tr [a-z] [A-Z]) |bc) "\a"
+    end
+  else
+    echo -n -e "\033]6;1;bg;*;default\a"
+  end
+end
+
+function cht
+  set -g title $argv
 end
